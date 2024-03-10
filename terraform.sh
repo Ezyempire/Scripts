9d4d46e00c893de1e088145d6b88581d3823ba9e
Ezyempire <156387732+Ezyempire@users.noreply.github.com>
2024-03-10 17:54:59 -0400

    Create terraform.sh
    terraform commands for configuring and installing with provider

---
+++ terraform.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if Terraform is already installed
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform 
+# Check the version of your installed terraform
+aws --version
+
+# Create a directory called terraform
+mkdir Terraform
+cd Terraform
+
+# Create a file called main.tf
+touch main.tf
+
+vim main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "Terraform-class" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+terraform apply
